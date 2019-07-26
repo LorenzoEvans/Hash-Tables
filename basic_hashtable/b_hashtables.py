548c4ae6092df28fe8239345f31151b6cd3cd785
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-07-25 22:14:28 -0400

    Honestly a little bit confused on how hashing helps target indexes.

--- basic_hashtable/b_hashtables.py
+++ basic_hashtable/b_hashtables.py
@@ -20,65 +20,67 @@ class BasicHashTable:
         self.elements = [None] * capacity
         pass
 
-    def djb2(key):
-        hash = 5381
-        for x in key:
-            hash = ((hash << 5) + hash) + ord(x)
-        return hash & 0xFFFFFFFF
     # '''
     # Fill this in.
     # Research and implement the djb2 hash function
     # '''
-    def hash(string, max):
-        hash_val = djb2(string) % max
-        return hash_val
+def hash(string, max):
+    hash = 5381
+    for x in string:
+        hash = ((hash << 5) + hash) + ord(x)
+        print("x is: ",x)
+        print("hash is: ",hash)
+    hash_val = hash % max & 0xFFFFFFFF
+    return hash_val
 
     # '''
     # Fill this in.
 
     # If you are overwriting a value with a different key, print a warning.
     # '''
-    def hash_table_insert(hash_table, key, value):
-        index = hash_table.capacity
-        pass
+def hash_table_insert(hash_table, key, value):
+    index = hash(key, hash_table.capacity)
+    print('hash_table.elements[index] was: ', hash_table.elements[index])
+    hash_table.elements[index] = value
+    print('index is: ',index)
+    print('value is: ',value)
+    print('hash_table.elements[index] is now: ', hash_table.elements[index])
+
+# '''
+# Fill this in.
+
+# If you try to remove a value that isn't there, print a warning.
+# '''
+def hash_table_remove(hash_table, key):
+    pass
 
 
-    # '''
-    # Fill this in.
+# '''
+# Fill this in.
 
-    # If you try to remove a value that isn't there, print a warning.
-    # '''
-    def hash_table_remove(hash_table, key):
-        pass
-
-
-    # '''
-    # Fill this in.
-
-    # Should return None if the key is not found.
-    # '''
-    def hash_table_retrieve(hash_table, key):
-        pass
+# Should return None if the key is not found.
+# '''
+def hash_table_retrieve(hash_table, key):
+    pass
 
 
-# def Testing():
-#     ht = BasicHashTable(16)
-#
-#     hash_table_insert(ht, "line", "Here today...\n")
-#
-#     hash_table_remove(ht, "line")
-#
-#     if hash_table_retrieve(ht, "line") is None:
-#         print("...gone tomorrow (success!)")
-#     else:
-#         print("ERROR:  STILL HERE")
-#
-#
-# Testing()
+def Testing():
+    ht = BasicHashTable(16)
+    print('hash is: ',hash("Grace Hopper", ht.capacity))
+    hash_table_insert(ht, "2384729842", "Grace Hopper")
+    print('elements are: ',ht.elements)
+    # hash_table_remove(ht, "line")
+    #
+    # if hash_table_retrieve(ht, "line") is None:
+    #     print("...gone tomorrow (success!)")
+    # else:
+    #     print("ERROR:  STILL HERE")
+
+
+Testing()
 
 
 
 
 
-
-print(hash("Hedera Hashgraph", 16))
+# print(hash("Hedera Hashgraph", 16))
